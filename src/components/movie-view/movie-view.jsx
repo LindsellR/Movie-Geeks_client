df39923568fb2832d85a27a1def3e1cd0432f52a
LindsellR <lindsellrichard@gmail.com>
2025-04-15 12:30:23 +1000

    react-bootstrap
    Imported react-bootstrap.
    
    Added react-bootstrap styling to login/sign-up, main-view, movie-view, movie-card, index.scss.
    
    Added movie-view.scss

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,51 +1,54 @@
+import PropTypes from "prop-types";
+import { Button } from "react-bootstrap";
+import "./movie-view.scss";
+
 export const MovieView = ({ movie, onBackClick }) => {
-  console.log("Movie recieved by MovieView:", movie)
-    return (
-      <div>
-        <div>
-          <img src={movie.image}
-          alt={movie.title} 
-          style={{ width: '200px', height: 'auto' }}
-          />
-        </div>
-        <br></br>
-        <div>
-          <strong><span>Title: </span></strong>
-          <span>{movie.title}</span>
-        </div>
-        <br></br>
-        <div>
-         <strong><span>Movie Description: </span></strong>
-            <span>{movie.description}</span>
-        </div>
-        <br></br>
-        <div>
-            <strong><span>Genre: </span></strong>
-            <span>{movie.genre}</span>
-            <p>{movie.genreDescription}</p>
-        </div>
-        <br></br>
-        <div>
-        <strong><span>Director: </span></strong>
-          <span>{movie.director}</span>
-          <p>{movie.bio}</p>
-        </div>
-        <br></br>
-        <div>
-        <strong> <span>Actors: </span></strong>
-          <ul>
-            {movie.actors && movie.actors.map((actor, index) => (
-              <li key={index}>{actor}</li>
-            ))}
-          </ul>
-</div>
-
-       
-
-
-        <br></br>
-        <button onClick={onBackClick}>Back</button>
+  return (
+    <div className="movie-view-container">
+      <div className="movie-image-container">
+        <img
+          src={movie.image}
+          alt={movie.title}
+          className="movie-image"
+        />
       </div>
-    );
-  };
-  
+      <br />
+      <div>
+        <h3>Title:</h3>
+        <span>{movie.title}</span>
+      </div>
+      <br />
+      <div>
+        <h3>Movie Description:</h3>
+        <span>{movie.description}</span>
+      </div>
+      <br />
+      <div>
+        <h3>Genre:</h3>
+        <span>{movie.genre}</span>
+        <p>{movie.genreDescription}</p>
+      </div>
+      <br />
+      <div>
+        <h3>Director:</h3>
+        <span>{movie.director}</span>
+        <p>{movie.bio}</p>
+      </div>
+      <br />
+      <div>
+        <h3>Actors:</h3>
+        <ul>
+          {movie.actors && movie.actors.map((actor, index) => (
+            <li key={index}>{actor}</li>
+          ))}
+        </ul>
+      </div>
+      <br />
+      <Button
+        className="back-button"
+        onClick={onBackClick}>
+        Back
+      </Button>
+    </div>
+  );
+};
--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {Form, Button, Card, CardGroup, Container, Col, Row} from "react-bootstrap"
 
 export const SignupView = () => {
   const [username, setUsername] = useState("");
@@ -32,44 +33,75 @@ export const SignupView = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Email:
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Birthday:
-        <input
-          type="date"
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+    <Container>
+      <Row>
+      {/* <Col></Col> */}
+        <Col>
+          <CardGroup>
+            <Card>
+              <Card.Header id="signup-heading">Sign Up for Movie Geeks</Card.Header>
+              <Card.Body>
+                <Form onSubmit={handleSubmit} aria-labelledby="signup-heading">
+                  <h2 ></h2>
+                  <Form.Group>
+                    <Form.Label htmlFor="signup-username">Username:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      required
+                      minLength="5"
+                      aria-required="true"
+                      placeholder="Username minimum 5 characters (Must be Alphanumeric)"
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label htmlFor="signup-password">Password:</Form.Label>
+                    <Form.Control
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                      minLength="8"
+                      aria-required="true"
+                      placeholder="Password Must Be At Least 8 Characters in Length"
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label htmlFor="signup-email">Email:</Form.Label>
+                    <Form.Control
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                      aria-required="true"
+                      placeholder="Please Enter a Valid Email Address"
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label htmlFor="signup-birthdate">Birthdate:</Form.Label>
+                    <Form.Control
+                      type="date"
+                      value={birthday}
+                      onChange={(e) => setBirthday(e.target.value)}
+                      required
+                      aria-required="true"
+                    />
+                  </Form.Group>
+                  <br></br>
+                  <Button type="submit" aria-label="Submit signup form">
+                    Sign Up
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </CardGroup>    
+        </Col>
+        {/* <Col></Col> */}
+      </Row>
+    </Container>
   );
 };
